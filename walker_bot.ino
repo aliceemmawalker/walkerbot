b228c47e32c9fb4efcf96bb2a3972dca43a130d7
Alice Emma Walker <walker.alice.e@gmail.com>
2016-12-10 12:24:08 +0800

    Adding basic code, walker bot

---
+++ walker_bot.ino
@@ -0,0 +1,29 @@
+// Include the Servo library 
+#include <Servo.h> 
+// Declare the Servo pin 
+int servoPin = 11; 
+int servoPin2 = 12;
+
+// Create a servo object 
+Servo Servo1; 
+Servo Servo2;
+void setup() { 
+   // We need to attach the servo to the used pin number 
+   Servo1.attach(servoPin); 
+   Servo2.attach(servoPin2);
+    pinMode(13, OUTPUT);
+}
+void loop(){ 
+     
+        
+  
+   // Make servo go to 0 degrees 
+   Servo1.write(0); 
+   Servo2.write(0); 
+   delay(500); 
+   // Make servo go to 90 degrees 
+   Servo1.write(40); 
+   Servo2.write(40); 
+   delay(500); 
+
+}
